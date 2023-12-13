649f45c777cabc5e64bdd0563ecc70dee58287a5
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-12-13 21:37:31 +0600

    Oparation Functions Done

---
+++ opararionFunction.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  list<int> myList = {10, 1, 200, 10, 12, 2, 1, 3, 12, 4, 5, 6, 7, 12};
+  // myList.remove(12);
+  // myList.sort();
+  // myList.sort(greater<int>());
+  // myList.unique();
+  // myList.reverse();
+  // cout << myList.front() << endl;
+  // cout << myList.back() << endl;
+  // cout << *next(myList.begin(), 2) << endl;
+  // for (int val : myList)
+  // {
+  //   cout << val << endl;
+  // }
+  return 0;
+}
